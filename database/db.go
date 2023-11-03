9f38be16eff585da391b46007f78a346f80a3318
aniket0951 <aniketsuryawanshixz1@gmail.com>
2023-11-03 22:44:35 +0530

    implement a docker for ad space service

--- database/db.go
+++ database/db.go
@@ -10,7 +10,7 @@ import (
 const DB_USERNAME = "root"
 const DB_PASSWORD = "root"
 const DB_NAME = "seller_app"
-const DB_HOST = "127.0.0.1"
+const DB_HOST = "host.docker.internal"
 const DB_PORT = "3306"
 
 var Db *gorm.DB
